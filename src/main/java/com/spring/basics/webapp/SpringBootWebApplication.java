30fc7999f6068487c8c9f083b8dd21bf3f011864
Alizada1353 <abdulrahim.alizada@powercoders.org>
2023-01-04 15:52:13 +0100

    created a simple jsp page

--- src/main/java/com/spring/basics/webapp/SpringBootWebApplication.java
+++ src/main/java/com/spring/basics/webapp/SpringBootWebApplication.java
@@ -1,9 +1,8 @@
 package com.spring.basics.webapp;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class SpringBootWebApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/spring/basics/webapp/controller/LoginController.java
+++ src/main/java/com/spring/basics/webapp/controller/LoginController.java
@@ -1,10 +1,13 @@
 package com.spring.basics.webapp.controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+
+@Controller
 public class LoginController {
-    @GetMapping("/login")
-    public String loginMessage(){
-        return "Hello World!";
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage(){
+        return "login";
     }
 }
